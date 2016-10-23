27334667fc580c6e0179d7364a7aab3ef48d04b3
pip_lineupbuilder <howley.phil@gmail.com>
2016-10-23 00:44:59 -0700

    add email to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ const Footer = (props) => {
   <p>This is a charity event. All proceeds go to <a href="https://stvincentmow.org/" target="_blank">St. Vincent{`'`}s Meals On Wheels for the Homeless</a>.</p>
   <a href="https://stvincentmow.org/" target="_blank"><img src="logo-svmow.png" alt="Meals On Wheels Logo" /></a>
   <SocialMedia />
+  <p>Email All Questions To: thehollywoodmoguls@gmail.com</p>
     </footer>
     )
 }
